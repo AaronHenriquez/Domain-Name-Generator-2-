36a4ae594d8e9c1ad887124219839d3cf051e945
Aaron Henriquez <axeliano01@gmail.com>
2022-12-12 05:55:54 +0000

    Segundo intento

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { library } from "webpack";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Array options
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let topLevelDomain = [".com", ".net", ".us", ".io", ".tv"];
+
+  //Empty array to assign Array options values
+  let domains = [];
+
+  //Cycles to push the Array Options into domains
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < topLevelDomain.length; l++) {
+          domains.push(pronoun[i] + adj[j] + noun[k] + topLevelDomain[l]);
+        }
+      }
+    }
+  }
+
+  console.log(domains);
+
+  document.getElementById("malditaListaDeDominios").innerHTML = domains;
 };
